ba9e1ae023a2c405c78a05ddf85db4b8cb8234d2
mpaulweeks <mpaulweeks@gmail.com>
2021-12-11 10:46:39 -0500

    draft get prev/next but broken, focus abs > fixed

--- src/gallery/Gallery.tsx
+++ src/gallery/Gallery.tsx
@@ -4,6 +4,7 @@ import { PhotoPreview } from "./PhotoPreview";
 import { Photo, PhotoSort } from "../util/types";
 import './Gallery.css';
 import { PhotoFocus } from "./PhotoFocus";
+import { getNextInArray, getPrevInArray } from "../util/util";
 
 interface GalleryProps {
   db: Database;
@@ -18,8 +19,23 @@ export function Gallery(props: GalleryProps) {
 
   const records = props.db.get(sortBy);
 
+  // todo does not work :(
+  function handleKeyDown(code: string) {
+    if (focused === undefined) { return; }
+    console.log(code);
+    if (code === 'ArrowLeft') {
+      setFocused(getPrevInArray(focused, records));
+    }
+    if (code === 'ArrowRight') {
+      setFocused(getNextInArray(focused, records));
+    }
+  };
+
   return (
-    <div className="GalleryContainer">
+    <div
+     className="GalleryContainer"
+     onKeyDown={evt => handleKeyDown(evt.code)}
+    >
       {records.map((p, i) => (
         <PhotoPreview
           key={i}
--- src/util/util.ts
+++ src/util/util.ts
@@ -12,3 +12,17 @@ export function sortBy<T, V extends Comparable>(arr: T[], cb: ((obj: T) => V)): 
   sorted.sort((a, b) => compare(cb(a), cb(b)));
   return sorted;
 }
+
+export function getPrevInArray<T>(elm: T, arr: T[], matcher?: (a: T, b: T) => boolean): T {
+  const matchingElm = matcher ? arr.filter(e => matcher(e, elm))[0] : undefined;
+  const index = arr.indexOf(matchingElm ?? elm);
+  const newIndex = (arr.length + index - 1) % arr.length;
+  return arr[newIndex];
+}
+
+export function getNextInArray<T>(elm: T, arr: T[], matcher?: (a: T, b: T) => boolean): T {
+  const matchingElm = matcher ? arr.filter(e => matcher(e, elm))[0] : undefined;
+  const index = arr.indexOf(matchingElm ?? elm);
+  const newIndex = (arr.length + index + 1) % arr.length;
+  return arr[newIndex];
+}
