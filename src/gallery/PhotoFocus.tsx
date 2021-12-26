5a2d00ef63b5ef41752d549b536239d47ea8b444
mpaulweeks <mpaulweeks@gmail.com>
2021-12-25 21:15:00 -0500

    polish focus styles

--- src/gallery/PhotoFocus.tsx
+++ src/gallery/PhotoFocus.tsx
@@ -12,11 +12,10 @@ export function PhotoFocus(props: PhotoFocusProps) {
     <div className="PhotoFocus" onClick={props.onExit}>
       <div
         className="PhotoFocusImage"
-        style={{backgroundImage: `url(${hiRes})`}}
+        style={{ backgroundImage: `url(${hiRes})` }}
       >
         &nbsp;
       </div>
-      <div className="PhotoFocusDate">{props.photo.date}</div>
       <div className="PhotoFocusDescription">{props.photo.description}</div>
     </div>
   )
