6a20d06e8236cab4a9fe10373d1e5eeb77775b0c
mpaulweeks <mpaulweeks@gmail.com>
2021-12-26 12:39:51 -0500

    split out search

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Database } from './util/db';
 import { Gallery } from './gallery';
+import { Search } from './navbar';
 
 export function App() {
   const [db, setDb] = useState<Database>();
-  const [searchRaw, setSearchRaw] = useState<string>('');
   const [searchTerms, setSearchTerms] = useState<string[]>([]);
 
   useEffect(() => {
@@ -16,10 +16,6 @@ export function App() {
     })();
   }, []);
 
-  function parseSearchTerms(raw: string) {
-    setSearchRaw(raw);
-    setSearchTerms(raw.split(' ').map(s => s.trim()));
-  }
 
   return (
     <div>
@@ -27,15 +23,11 @@ export function App() {
         <h1>
           Rockland Fan Site
         </h1>
-        <input
-          placeholder="Type here to search"
-          value={searchRaw}
-          onChange={evt => parseSearchTerms(evt.target.value)}
-        />
+        <Search setSearchTerms={setSearchTerms} />
       </header>
       {db ? (
         <Gallery db={db} searchTerms={searchTerms} />
-      ):(
+      ) : (
         <h1>loading...</h1>
       )}
     </div>
---
+++ src/navbar/Search.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import './Search.css';
+
+interface SearchProps {
+  setSearchTerms(terms: string[]): void;
+}
+
+export function Search(props: SearchProps) {
+  const [searchRaw, setSearchRaw] = useState<string>('');
+
+  function parseSearchTerms(raw: string) {
+    setSearchRaw(raw);
+    props.setSearchTerms(raw.split(' ').map(s => s.trim()).filter(s => !!s));
+  }
+
+  return (
+    <input
+      className="Search"
+      placeholder="Type here to search"
+      value={searchRaw}
+      onChange={evt => parseSearchTerms(evt.target.value)}
+    />
+  )
+}
